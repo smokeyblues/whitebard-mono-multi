a7849978b248a25d1d1c7899149549ae1b29997a
smokeyblues <smokeyblues@CORP.AMAZONWORKSPACES.COM>
2019-03-20 13:22:14 -0600

    making a mistake... FOR SCIENCE!

--- create.js
+++ create.js
@@ -1,6 +1,7 @@
 import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
+gibberish.what;
 
 export async function main(event, context) {
     const data = JSON.parse(event.body);
